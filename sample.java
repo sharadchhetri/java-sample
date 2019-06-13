c3efe57b798e325a08945457902b0e4abd8bbda5
Sharad Chettri <sharadchhetri@users.noreply.github.com>
2019-06-13 13:36:07 +0530

    update

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 class sample
 {
 	public static void public static void main(String[] args) {
-		System.out.println("This is sample java code")
+		System.out.println("This is sample java code");
 	}
 }
